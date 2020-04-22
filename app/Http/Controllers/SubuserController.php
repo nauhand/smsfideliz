17b679bb0fb51585ff88e4513a61b1712d574e4c
nauhand <nauhand00@gmail.com>
2020-04-22 12:45:35 +0000

    a
    a

---
+++ app/Http/Controllers/SubuserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Subuser;
+use Illuminate\Http\Request;
+
+class SubuserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Subuser  $subuser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subuser $subuser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Subuser  $subuser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subuser $subuser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Subuser  $subuser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subuser $subuser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Subuser  $subuser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subuser $subuser)
+    {
+        //
+    }
+}
